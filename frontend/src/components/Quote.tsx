c39ac979a68022a32e7c904edba7fac512a14738
Ng Cheenglemba Singha <135134524+ngcheng137@users.noreply.github.com>
2024-07-30 13:58:03 +0530

    Update Quote.tsx

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -2,7 +2,7 @@ export const Quote = () => {
     return <div className="bg-slate-700 h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <div className="max-w-md text-center text-5xl font-bold text-white">
-                Hello from PALLARBOND!!!
+                Hello and Welcome!
             </div>  
         </div>
     </div>
